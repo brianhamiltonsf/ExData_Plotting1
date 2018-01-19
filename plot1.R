6b0634c6865a8b0664bebf381386e14b6f912767
Brian <brianhamiltonsf@gmail.com>
2018-01-18 21:59:06 -0800

    Add plot 1 files

---
+++ plot1.R
@@ -0,0 +1,8 @@
+library(lubridate)
+data <- read.table("./data/household_power_consumption.txt", header = TRUE, sep=";", na.strings = "?", stringsAsFactors = FALSE)
+data$Date <- dmy(data$Date)
+data <- subset(data, data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
+par(bg = "white")
+hist(data$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power", bg = "white")
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
